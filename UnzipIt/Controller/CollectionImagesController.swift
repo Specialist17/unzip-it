057200fe453e99d3c2c0fcccf0e3e0b1c8b8e9c2
Specialist17 <arocho.fernando.13@gmail.com>
2017-11-02 20:59:59 -0700

    about to implement images view

---
+++ UnzipIt/Controller/CollectionImagesController.swift
@@ -0,0 +1,48 @@
+//
+//  CollectionImagesController.swift
+//  UnzipIt
+//
+//  Created by Fernando on 11/2/17.
+//  Copyright © 2017 Specialist. All rights reserved.
+//
+
+import UIKit
+
+class CollectionImagesController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
+extension CollectionImagesController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 9
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    
+}
